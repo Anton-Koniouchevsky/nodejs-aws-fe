b05cc33db148fb3ec31dbbcedc75cb6a4678b5bd
Anton Koniouchevski <Anton_Koniouchevski@epam.com>
2021-05-03 00:08:53 +0300

    integrate UI with import-service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://0es8pfioli.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://te140u5ox0.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };

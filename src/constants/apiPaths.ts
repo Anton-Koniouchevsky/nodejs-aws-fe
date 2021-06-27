a78b07780783cf19c57f1fd48cdb52a73866a8d9
Anton Koniouchevski <Anton_Koniouchevski@epam.com>
2021-06-27 22:21:48 +0300

    add cart api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://if4nsvb850.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://te140u5ox0.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://anton-koniouchevski-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
